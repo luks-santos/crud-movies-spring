a7395362622b4f16116481aace2d7df2e95ad7d9
Lucas Batista <lucas.bt46@gmail.com>
2023-10-23 16:55:47 -0300

    configs conexao front angular

--- src/main/java/com/lucas/crud/entities/Movie.java
+++ src/main/java/com/lucas/crud/entities/Movie.java
@@ -1,5 +1,6 @@
 package com.lucas.crud.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @JsonProperty("_id")
     private UUID id;
 
     @Column(length = 200, nullable = false)
