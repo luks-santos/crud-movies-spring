6089d06559d65214042c1372ed9341feaafd8e82
Lucas Batista <lucas.bt46@gmail.com>
2023-10-23 13:21:26 -0300

    projeto criado

---
+++ src/main/java/com/lucas/crud/CrudMoviesTvserieSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMoviesTvserieSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudMoviesTvserieSpringApplication.class, args);
+    }
+
+}
