5dbec926c5f8210406d57ecb49ad34703eb801ef
Lucas Batista <lucas.bt46@gmail.com>
2023-10-25 17:12:56 -0300

    Adicionar ResponseStatus post movies

--- src/main/java/com/lucas/crud/controller/MoviesController.java
+++ src/main/java/com/lucas/crud/controller/MoviesController.java
@@ -4,6 +4,9 @@ import com.lucas.crud.entities.Movie;
 import com.lucas.crud.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +25,9 @@ public class MoviesController {
     }
 
     @PostMapping
-    public Movie save(@RequestBody Movie movie) { return movieService.save(movie); }
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Movie save(@RequestBody Movie movie) {
+        //return ResponseEntity.status(HttpStatus.CREATED).body(this.movieService.save(movie));
+        return this.movieService.save(movie);
+    }
 }
